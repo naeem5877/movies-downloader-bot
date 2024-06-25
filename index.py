066958e5b9106e969db1186e78f2d2af0435e456
Naeem Ahmed <130828773+naeem5877@users.noreply.github.com>
2024-06-25 14:25:27 +0600

    Update index.py

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryH
 from movies_scraper import search_movies, get_movie
 
 TOKEN = os.getenv("TOKEN")
-URL = os.getenv("URL")
+URL = "https://movies-prime-bot.onrender.com"
 bot = Bot(TOKEN)
 
 def welcome(update, context):
